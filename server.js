5e4df074a283782790de27e1b996e992ca23db15
chanka-herath2001 <cb010779@students.apiit.lk>
2026-02-21 20:59:52 +0530

    new cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://enoth.vercel.app/',
+  origin: process.env.FRONTEND_URL || 'https://enoth.vercel.app',
   credentials: true
 }));
 
